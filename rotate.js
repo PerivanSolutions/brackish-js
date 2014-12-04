878c719d76829c4e0b45845e2f82b6b5e4b6c0de
Louis Pilfold <louis@lpil.uk>
2014-12-04 13:47:56 +0000

    Rotate: Get els from scope rather than passing

--- rotate.js
+++ rotate.js
@@ -6,24 +6,24 @@
       rotateBackward,
       els = document.getElementsByClassName('js-rotate__content');
 
-  rotateForward = function rotateForward(nodes) {
-    nodes[0].parentNode.appendChild(nodes[0]);
+  rotateForward = function rotateForward() {
+    els[0].parentNode.appendChild(els[0]);
   };
 
-  rotateBackward = function rotateBackward(nodes) {
+  rotateBackward = function rotateBackward() {
     // Place the last before the second
-    nodes[0].parentNode.insertBefore(nodes[nodes.length - 1], nodes[1]);
+    els[0].parentNode.insertBefore(els[els.length - 1], els[1]);
 
     // Place the first before the third
     window.setTimeout(function() {
-      nodes[0].parentNode.insertBefore(nodes[0], nodes[2]);
+      els[0].parentNode.insertBefore(els[0], els[2]);
     }, 100);
   };
 
   autoRotate = function autoRotate() {
     window.clearInterval(rotateInterval);
     rotateInterval =
-      window.setInterval(function() { rotateForward(els); }, 8000);
+      window.setInterval(function() { rotateForward(); }, 8000);
     return rotateInterval;
   };
 
